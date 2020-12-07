d487d92286f20dd8a80cefa300c89066fa519b88
Mike Straw <mike.straw@automattic.com>
2020-12-07 10:11:21 -0500

    Day 5 Part 1 done. Added source code link
    Still working on day 5, but added a link back to the GH repo

--- 5/part1.js
+++ 5/part1.js
@@ -1,4 +1,40 @@
 solvers.push(new Problem( () => {
-    const input = document.querySelector('#sample').innerText.split('\n');
-    return('NOT READY');
+    const input = document.querySelector('#input').innerText.split('\n');
+
+    var maxSeatId = 0;
+
+    for ( let i = 0; i < input.length; i++ ) {
+        let rows = Array.from(Array(128).keys()),
+        columns = Array.from(Array(8).keys()),
+        seat = input[i];
+
+        for ( let j = 0; j < seat.length; j++ ) {
+            indicator = seat[j];
+            if ( indicator === 'F' || indicator === 'B' ) {
+                rows = checkSeat( indicator, rows );
+            } else {
+                columns = checkSeat( indicator, columns );
+            }
+        }
+
+        seatId = rows[0] * 8 + columns[0];
+
+        console.log(`${seat}: Row ${rows[0]}, Column ${columns[0]}, seat ID ${seatId}`);
+
+        if ( seatId > maxSeatId ) {
+            maxSeatId = seatId;
+        }
+    }
+    return(maxSeatId);
 }));
+
+function checkSeat( indicator, range ) {
+    if ( indicator === 'F' || indicator === 'L' ) {
+        return range.slice(0, range.length / 2);
+    } else if ( indicator === 'B' || indicator === 'R' ) {
+        return range.slice(range.length / 2);
+    } else {
+        // If we're here, it's bad
+        return false;
+    }
+}
