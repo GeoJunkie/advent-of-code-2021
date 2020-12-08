51c12c91b1169b22c7a8f1f80c713d09a05fb894
Mike Straw <mike.straw@automattic.com>
2020-12-08 07:03:14 -0500

    Day 6 completed

--- 6/part1.js
+++ 6/part1.js
@@ -1,4 +1,21 @@
-solvers.push(new Problem( () => {
-    const input = document.querySelector('#sample').innerText.split('\n');
-    return('NOT READY');
-}));
+solvers.push(
+  new Problem(() => {
+    const input = document.querySelector("#input").innerText.split("\n\n");
+
+    let sum = 0;
+
+    for (i = 0; i < input.length; i++) {
+      const group = input[i];
+      let answers = {};
+
+      for (j = 0; j < group.length; j++) {
+          question = group[j];
+        if (question !== "\n") {
+          answers[question] = true;
+        }
+      }
+      sum += Object.keys(answers).length;
+    }
+    return sum;
+  })
+);
--- 6/part2.js
+++ 6/part2.js
@@ -1,4 +1,32 @@
-solvers.push(new Problem( () => {
-    const input = document.querySelector('#sample').innerText.split('\n');
-    return('NOT READY');
-}));
+solvers.push(
+  new Problem(() => {
+    const input = document.querySelector("#input").innerText.split("\n\n");
+
+    let sum = 0;
+
+    for (let i = 0; i < input.length; i++) {
+      const group = input[i],
+        people = group.split("\n");
+      let answers = {};
+
+      for (let j = 0; j < people.length; j++) {
+        const person = people[j];
+        for (let k = 0; k < person.length; k++) {
+          const answer = person[k];
+          if (answers[answer]) {
+            answers[answer] += 1;
+          } else {
+            answers[answer] = 1;
+          }
+        }
+      }
+      const keys = Object.keys(answers);
+      for (l = 0; l < keys.length; l++) {
+        if (answers[keys[l]] === people.length) {
+          sum++;
+        }
+      }
+    }
+    return sum;
+  })
+);
