1f88799db5d1913cce62c562dc3c4a51c2f9a33e
Mike Straw <mike.straw@automattic.com>
2020-12-17 07:21:22 -0500

    Completed Day 10

--- 10/part2.js
+++ 10/part2.js
@@ -26,13 +26,59 @@ solvers.push(new Problem( (rawInput) => {
      * ratings[0] = true
      * ratings[n] - ratings[n-1] <= 3
      * 
+     * This function uses the bottom-up approach
+     * 
+     * Inspiration: https://www.geeksforgeeks.org/count-possible-paths-source-destination-exactly-k-edges/
+     * 
+     * "The idea is to build a 3D table where first dimension is source, second dimension is destination, third dimension is number of edges from source to destination, and the value is count of walks."
+     * 
+     * 
+     * Logic:
+     * 
+     * You can determine total paths by summing the paths of each incoming link
+     * 
+     * Graphic Example: https://d.pr/i/VK6yPV
+     * 
+     * For each number
+     *  Find possible incoming paths (every number where this - previous <= 3)
+     *  Sum each previous path count
+     * 
+     * Assumptions:
+     * 
+     * The list is sorted
+     * There is a possible chain all the way through (skipping error checking for the puzzle)
+     * 
+     * @param {integer array} list       The numbers to check (sorted)
+    */
+    function countChains( list ) {
+        const chains = [1];
+
+        for (let index = 1; index < list.length; index++) {
+            // Step backwards to find all incoming paths that meet the criteria
+            let lookBack = index - 1,
+                sum = 0;
+            while ( list[index] - list[lookBack] <= 3 ) {
+                sum += chains[lookBack];
+                lookBack--;
+            }
+            chains[index] = sum;
+        }
+        return chains[chains.length - 1];
+    }
+
+
+    /** 
+     * Counts the number of possible chains where the following is true for each item:
+     * ratings[0] = true
+     * ratings[n] - ratings[n-1] <= 3
+     * 
      * This function uses memoization
      * 
      * @param {integer array} list       The numbers to check (sorted)
      * @param {integer} index               The index of the current rating in the chain
      * @param {integer array} chainCounts   Array of counts found so far for each element 
     */
-    function countChains( list, index = 0, chainCounts = [] ) {
+    function countChainsMemoization( list, index = 0, chainCounts = [] ) {
         if (chainCounts[index] !== undefined ) {
             return chainCounts[index];
         } else if ( list.length === 1 ) {
